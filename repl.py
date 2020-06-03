d3b315f6617230bcc891da26f17ccff055809694
Smital Desai <desaismital@gmail.com>
2020-06-03 14:40:37 +0530

    Add support for parsing in repl and add monkey face

--- repl.py
+++ repl.py
@@ -6,24 +6,41 @@ from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.completion import WordCompleter
 from pygments.lexers import load_lexer_from_file
+from parser import Parser
 
+MONKEY = """"\
+            __,__
+   .--.  .-"     "-.  .--. 
+  / .. \/  .-. .-.  \/ ..  |
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"`` ``"-./, -' /
+   `'-' /_   ^ ^   _\ '-'`
+       |  \._   _./  |
+       \   \ `~` /   /
+       '._ '-=-' _.'
+           '~---~'
+       """
 
 def main(argv):
     PROMPT = ">> "
-    print(f"Hello {getpass.getuser()} This is the Monkey programming language!\n")
+    print(f"Hello {getpass.getuser()}, This is the Monkey programming language!\n")
+    print(f"{MONKEY}")
     print(f"Feel free to type in commands")
     suggestions = WordCompleter([item for item in monkey_lexer.keywords], ignore_case=True)
-    lexer = load_lexer_from_file("/Users/smital/PycharmProjects/writing-interpreter-in-python/monkey_pyg_lexer.py", "MonkeyLexer")
+    lexer = load_lexer_from_file("/Users/smital/PycharmProjects/writing-interpreter-in-python/monkey_pyg_lexer.py",
+                                 "MonkeyLexer")
     while True:
         # TODO: lexer=lexer Get the custom lexer working for monkey language
         data = prompt(f"{PROMPT}",
                       history=FileHistory('history.txt'),
                       auto_suggest=AutoSuggestFromHistory(),
                       completer=suggestions,
-                      multiline=True)
+                      multiline=False)
         lexer = monkey_lexer.Lexer(data)
-        for item in lexer:
-            print(item)
+        parser = Parser.new(lexer)
+        program = parser.parse()
+        print(program)
 
 
 if __name__ == '__main__':
--- trace_helper.py
+++ trace_helper.py
@@ -8,10 +8,11 @@ class TraceCalls(object):
         to their call depth.
     """
 
-    def __init__(self, stream=sys.stdout, indent_step=4, show_ret=False):
+    def __init__(self, stream=sys.stdout, indent_step=4, show_ret=False, debug=False):
         self.stream = stream
         self.indent_step = indent_step
         self.show_ret = show_ret
+        self.debug = debug
 
         # This is a class attribute since we want to share the indentation
         # level between different traced functions, in case they call
@@ -21,19 +22,22 @@ class TraceCalls(object):
     def __call__(self, fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            indent = ' ' * TraceCalls.cur_indent
-            argstr = ', '.join(
-                [str(a) for a in args[1:]] +
-                ["%s=%s" % (a, repr(b)) for a, b in kwargs.items()])
-            self.stream.write('\n%s BEGIN %s(%s)\n' % (indent, fn.__name__, argstr))
+            if self.debug:
+                indent = ' ' * TraceCalls.cur_indent
+                argstr = ', '.join(
+                    [str(a) for a in args[1:]] +
+                    ["%s=%s" % (a, repr(b)) for a, b in kwargs.items()])
+                self.stream.write('\n%s BEGIN %s(%s)\n' % (indent, fn.__name__, argstr))
 
-            TraceCalls.cur_indent += self.indent_step
+                TraceCalls.cur_indent += self.indent_step
             ret = fn(*args, **kwargs)
-            TraceCalls.cur_indent -= self.indent_step
-            self.stream.write('%s END %s(%s)\n\n' % (indent, fn.__name__, argstr))
 
-            if self.show_ret:
-                self.stream.write('%s--> %s\n' % (indent, ret))
+            if self.debug:
+                TraceCalls.cur_indent -= self.indent_step
+                self.stream.write('%s END %s(%s)\n\n' % (indent, fn.__name__, argstr))
+
+                if self.show_ret:
+                    self.stream.write('%s--> %s\n' % (indent, ret))
             return ret
 
         return wrapper
